9a7b8ec63380deddc6383d39298c6923f5e3a2d3
kallistahalim <kallistahalim@gmail.com>
2020-05-20 11:07:26 -0700

    misspell oops lol

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var MatLibs = [
     "It\'s [adjective] that we don't know what we've got until we [verb] it, but it's also true that we don\'t know what we\'ve been missing until [noun] arrives",
     "The average [noun] would rather have beauty than brains, because the average man can [verb] [adjective] than he can think",
     "USA Today has [verb] out with a new survey: Apparently three out of four people make up 75 percent of the [adjective] [noun]",
-    "You have the right to [verb] [adjective]. Anything you say will be misquoted, then used against[noun]"
+    "You have the right to [verb] [adjective]. Anything you say will be misquoted, then used against [noun]"
 ]
 
 
